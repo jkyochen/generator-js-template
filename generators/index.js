e5c3f9917b0ff3c7e6fed14bcbb3aa9a06a44385
Lanlyhs <ziqiang.chen@outlook.com>
2023-03-03 19:36:59 +0900

    Update prepare for support typescript

--- generators/index.js
+++ generators/index.js
@@ -5,17 +5,32 @@ const Generator = require("yeoman-generator");
 module.exports = class extends Generator {
 	constructor(args, opts) {
 		super(args, opts);
+		this.option("isNeedTypeScript", {
+			type: Boolean,
+			description: "Project type, default is javscript",
+		});
 	}
 
 	initializing() {
 		this.spawnCommandSync("git", ["init", "--quiet"]);
 	}
 
+	async prompting() {
+		this.answers = await this.prompt({
+			type: "confirm",
+			name: "isNeedTypeScript",
+			message: "Need TypeScirpt proejct?",
+			default: false,
+		});
+	}
+
 	writing() {
-		this.fs.copy(
-			[this.templatePath("."), this.templatePath(".husky")],
-			this.destinationPath("."),
-		);
+		if (this.answers.isNeedTypeScript) {
+			this.fs.copy(this.templatePath("ts"), this.destinationPath("."));
+		} else {
+			this.fs.copy(this.templatePath("js"), this.destinationPath("."));
+		}
+		this.fs.copy(this.templatePath(".husky"), this.destinationPath(".husky"));
 		this.fs.copy(
 			this.templatePath(".gitignore.copy"),
 			this.destinationPath(".gitignore"),
